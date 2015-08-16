b709492eaba18fbbbcb927361285f2fdf66f78f4
James Halliday <mail@substack.net>
2015-08-16 12:11:54 -0700

    example

---
+++ example/add.js
@@ -0,0 +1,10 @@
+var hyperlog = require('hyperlog')
+var memdb = require('memdb')
+var hsodium = require('../')
+var sodium = require('sodium')
+
+var keypair = sodium.api.crypto_sign_keypair()
+var log = hyperlog(memdb(), hsodium(sodium, keypair))
+log.add('whatever', function (err, node) {
+  console.log(node)
+})
