5c6ef93e765c7fd00056f7eb3f0efc4bc7b35939
James Halliday <mail@substack.net>
2015-08-16 12:31:56 -0700

    passing test

---
+++ test/add.js
@@ -0,0 +1,18 @@
+var hyperlog = require('hyperlog')
+var memdb = require('memdb')
+var hsodium = require('../')
+var sodium = require('sodium')
+var test = require('tape')
+
+test('add', function (t) {
+  t.plan(3)
+  var keypair = sodium.api.crypto_sign_keypair()
+  var log = hyperlog(memdb(), hsodium(sodium, keypair))
+
+  log.add(null, Buffer('whatever'), function (err, node) {
+    t.ifError(err)
+    t.deepEqual(node.identity, keypair.publicKey)
+    var m = sodium.api.crypto_sign_open(node.signature, node.identity)
+    t.deepEqual(m, Buffer(node.key, 'hex'))
+  })
+})
