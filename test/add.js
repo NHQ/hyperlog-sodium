47c0af9624b6a25cbc895153aa7536c53f902bac
James Halliday <mail@substack.net>
2015-08-16 12:32:17 -0700

    api test

--- test/add.js
+++ test/add.js
@@ -16,3 +16,16 @@ test('add', function (t) {
     t.deepEqual(m, Buffer(node.key, 'hex'))
   })
 })
+
+test('add api', function (t) {
+  t.plan(3)
+  var keypair = sodium.api.crypto_sign_keypair()
+  var log = hyperlog(memdb(), hsodium(sodium.api, keypair))
+
+  log.add(null, Buffer('whatever'), function (err, node) {
+    t.ifError(err)
+    t.deepEqual(node.identity, keypair.publicKey)
+    var m = sodium.api.crypto_sign_open(node.signature, node.identity)
+    t.deepEqual(m, Buffer(node.key, 'hex'))
+  })
+})
