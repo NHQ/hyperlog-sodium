be84292489a9e2c83bd82f5b16f109840675e881
James Halliday <mail@substack.net>
2015-08-16 13:29:10 -0700

    working with arrays

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var eq = require('buffer-equals')
 var xtend = require('xtend')
+var isarray = require('isarray')
+var defined = require('defined')
 
 module.exports = function (sodium, keypair, opts) {
   if (sodium.api && !sodium.crypto_sign) sodium = sodium.api
   return xtend({
-    identity: keypair.publicKey,
+    identity: defined(keypair.identity, keypair.publicKey),
     sign: function (node, cb) {
       var bkey = Buffer(node.key, 'hex')
       cb(null, sodium.crypto_sign(bkey, keypair.secretKey))
     },
     verify: function (node, cb) {
       var m = sodium.crypto_sign_open(node.signature, node.identity)
-      var bkey = Buffer(node.key, 'hex')
-      cb(null, eq(m, keypair.publicKey))
+      if (isarray(keypair.publicKey)) {
+        for (var i = 0; i < keypair.publicKey.length; i++) {
+          if (eq(node.identity, keypair.publicKey[i])) break
+        }
+        if (i === keypair.publicKey.length) return cb(null, false)
+      }
+      else if (!eq(node.isarray, keypair.publicKey)) cb(null, false)
+ 
+      cb(null, eq(m, Buffer(node.key, 'hex')))
     }
   }, opts)
 }
